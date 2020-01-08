aea51f738818bbc3a587af93932e738d832f6002
saumya <2saumyaray@gmail.com>
2020-01-08 17:51:02 +0530

    UI development

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
+//
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 //
@@ -10,21 +10,9 @@ import InfoComp from './comps/info.comp';
 function App() {
   return (
     <div className="App">
-      <HomeComp name="One" />
-      <InfoComp name="One" />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <HomeComp name="One" />
+        <InfoComp name="One" />
       </header>
     </div>
   );
