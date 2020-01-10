c6c4351beaa0f9f207c03f30bd8b1b8b80a8caa2
saumya <2saumyaray@gmail.com>
2020-01-10 14:37:44 +0530

    Example Component for settings is added

---
+++ src/comps/app.configs.example.js
@@ -0,0 +1,12 @@
+//
+// visibility : public, unlisted, private, direct
+//
+const AppConfig = {
+    'mastodon':{
+        'uri' : 'https://mastodon.mine',
+        'accessToken' : 'API_accesstoken_from_app_settings',
+        'statusVisibility' : 'public',
+    }
+}
+
+export default AppConfig;
