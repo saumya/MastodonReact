e6b85051f242548cadf2288bf408fad7d0e947cb
saumya <2saumyaray@gmail.com>
2020-01-11 13:03:50 +0530

    OAuth flow for loggin in

--- src/comps/mastodon.comp.js
+++ src/comps/mastodon.comp.js
@@ -7,11 +7,14 @@ const MastodonComp = function(props){
 
     //const testProp = 'What?!';
 
+    const mastodon_instance = 'https://mastodon.social';
+
     const onGotMastondonPublicUpdates = function(updates){
         console.log( 'onGotMastondonPublicUpdates' );
-        console.log( updates );
+        //console.log( updates );
         updates.map(function(item){
             console.log('Status', item.content);
+            return item;
         });
     }
     const onTestButtonClick = function(event){
@@ -44,6 +47,30 @@ const MastodonComp = function(props){
         //
         return false;
     }
+    //
+    // Auth
+    const onLoginButtonClick = function(event){
+        event.preventDefault();
+        //
+        
+        return false;
+    }
+    // Auth /
+    const onLoginClick_2 = function(event){
+        //
+        const uri_auth = mastodon_instance + '/oauth/authorize';
+        const uri_auth_q = uri_auth+'?client_id=YOUR_CLINET_ID&scope=read+write+follow&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code';
+        // Ridirect to the URL 
+        // Get the Code
+        // Ridirect back here
+        //
+        //
+        window.location.href = uri_auth_q;
+        //window.location.replace(uri_auth); //removes from history, so no back button
+        //
+        return false;
+    }
+    //
     return(
         <React.Fragment>
             <div className="container">
@@ -51,6 +78,9 @@ const MastodonComp = function(props){
                 <h2 className="subTitle">API explorer</h2>
                 <div>
                     <button className="button is-primary" onClick={onTestButtonClick}>Test Mastodon</button>
+                </div>
+                <div className="box">
+                    <button className="button is-info" onClick={ onLoginClick_2 }>Login</button>
                 </div>
             </div>
         </React.Fragment>
