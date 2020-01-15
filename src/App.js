5219f36bd5c3dfbfac6201af929cdf0a381cda24
saumya <2saumyaray@gmail.com>
2020-01-15 13:17:39 +0530

    UI Navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 //
@@ -10,14 +10,54 @@ import MastodonComp from './comps/mastodon.comp';
 //
 
 function App() {
+
+  // home, mastodon, auth0
+  const [ uiName, setUIName ] = useState('home');
+  useEffect(()=>{
+    // Nothing Yet!
+  });
+
+  //
+  const renderUIForState = ()=>{
+    let uiCompToRender = null;
+    if(uiName === 'home'){
+      uiCompToRender = ( <ContactComp /> );
+    }else if( uiName === 'mastodon' ){
+      uiCompToRender = ( <MastodonComp /> );
+    }else if( uiName === 'auth0' ){
+      uiCompToRender = ( <div>TODO</div> );
+    }
+    return uiCompToRender;
+  }
+  //
+
+  const onHomeClick = () => {
+    setUIName('home');
+  }
+  const onMastodonClick = () => {
+    setUIName('mastodon');
+  }
+  const onAuth0Click = () => {
+    setUIName('auth0');
+  }
+
+  //console.log('uiToRender:', uiToRender);
+
   return (
     <section className="section">
       <div className="container">
+        
         <header>
           <HomeComp name="One" />
           <InfoComp name="One" />
-          <ContactComp />
-          <MastodonComp />
+
+          <button className="button" onClick={onHomeClick}>Home</button>
+          <button className="button" onClick={onMastodonClick}>Mastodon</button>
+          <button className="button" onClick={onAuth0Click}>Auth0</button>
+
+          {/* ( ( uiName==='mastodon' ) ? <MastodonComp /> : <ContactComp /> ) */}
+          { renderUIForState() }
+
         </header>
       </div>
     </section>
